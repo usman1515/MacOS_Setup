0bd869fe677f1211a634d270914029299e92c8b8
usman1515 <usmansiddique1515@gmail.com>
2022-12-15 17:01:21 +0500

    created bash script install home brew

---
+++ install_homebrew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~
+
+# pre-req - install command line tools for xcode
+sudo xcode-select --install
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# check if all is ok
+brew doctor
+
+# check brew directory
+ls -a /usr/local/Homebrew/
+brew
