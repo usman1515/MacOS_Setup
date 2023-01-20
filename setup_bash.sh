83c354440e9667a013eae03bd3a183eecb36e875
usman1515 <usmansiddique1515@gmail.com>
2023-01-21 00:10:38 +0500

    updated script install_bash

--- setup_bash.sh
+++ setup_bash.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Dont use pre installed bash as its outdated.
-brew uninstall bash
-
 # install bash
 brew install bash
 # install bash auto completion
-brew install bash-completion
+brew install bash-completion@2
 
 # determine which bash got installed
 which bash
-# this will likely print: /usr/local/bin/bash
 
 # add the new bash shell path to /etc/shells
-sudo sh -c 'echo /usr/local/bin/bash >> /etc/shells'
+sudo sh -c 'echo /opt/homebrew/bin/bash >> /etc/shells'
 
 # list all shells
 cat /etc/shells
@@ -22,17 +18,12 @@ echo -e "Currently using shell: " $SHELL
 echo -e ""
 
 # change shell to bash
-chsh -s /usr/local/bin/bash
+chsh -s /opt/homebrew/bin/bash
 echo -e "Now using shell:       " $SHELL
 bash --version
 
-# copy bash profile and bash aliases
-cp -rv configs/.bash_profile ~/.bash_profile
-cp -rv configs/.bash_aliases ~/.bash_aliases
-
 # update setings
 source ~/.bash_profile
-
 
 
 # to learn more about the basic operations of the bash shell consult the following repo.
