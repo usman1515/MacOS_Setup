06ff2400419b1eb8101453671115c2a2f22dfb0b
usman1515 <usmansiddique1515@gmail.com>
2023-09-14 22:58:51 +0500

    created bash script setup zsh

---
+++ setup_zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~
+
+# download oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# download zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# download zsh auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# download powerlevel 10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
